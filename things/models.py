bad814eb63f4291499a38037223e11112c148669
Harry Sharma <harshit.sharma@kcl.ac.uk>
2022-10-09 00:19:16 +0100

    fix?

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from enum import unique
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-def Thing (AbstractUser):
+class Thing(AbstractUser):
     name =models.CharField(unique=True, blank=True,max_length=30)
     description= models.TextField(blank=False, max_length=120)
     quantity=models.IntegerField()
