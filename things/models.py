4157a533cb0ee9a9709d853f87b233aaf4778421
Harry Sharma <harshit.sharma@kcl.ac.uk>
2022-10-09 18:09:12 +0100

    Task 2 finish

--- things/models.py
+++ things/models.py
@@ -1,9 +1,12 @@
 from enum import unique
+from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Thing(AbstractUser):
-    name =models.CharField(unique=True, blank=True,max_length=30)
-    description= models.TextField(blank=False, max_length=120)
+    name =models.CharField(max_length=30)
+    description= models.TextField()
     quantity=models.IntegerField()
+    username=models.CharField(blank=True, unique=True, max_length=1)
+    password=models.CharField(blank=True, max_length=2)
     
