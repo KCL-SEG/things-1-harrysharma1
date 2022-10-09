c6c321f4fa155fffeb4fbe0411ea423d96f559cb
Harry Sharma <harshit.sharma@kcl.ac.uk>
2022-10-09 18:51:56 +0100

    Add validations

--- things/models.py
+++ things/models.py
@@ -1,12 +1,16 @@
-from enum import unique
-from pyexpat import model
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(AbstractUser):
-    name =models.CharField(max_length=30)
-    description= models.TextField()
-    quantity=models.IntegerField()
+    name =models.CharField(max_length=30,unique=True)
+    description= models.TextField(max_length=120,unique=False,blank=False)
+    quantity=models.IntegerField(
+        unique=False,
+        default=0,
+        validators=[MaxValueValidator(100),MinValueValidator(0)]
+    )
     username=models.CharField(blank=True, unique=True, max_length=1)
     password=models.CharField(blank=True, max_length=2)
     
