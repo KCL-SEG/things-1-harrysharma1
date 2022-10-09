a5480c81a380859c7fd8d48dd9567a5e6171cff3
Harry Sharma <harshit.sharma@kcl.ac.uk>
2022-10-09 19:47:59 +0100

    Reduce fails to only 2

--- things/models.py
+++ things/models.py
@@ -5,12 +5,11 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(AbstractUser):
     name =models.CharField(max_length=30,unique=True)
-    description= models.TextField(max_length=120,unique=False,blank=False)
+    description= models.CharField(max_length=120,blank=True)
     quantity=models.IntegerField(
-        unique=False,
         default=0,
         validators=[MaxValueValidator(100),MinValueValidator(0)]
     )
-    username=models.CharField(blank=True, unique=True, max_length=1)
-    password=models.CharField(blank=True, max_length=2)
+    username=models.CharField(blank=True, unique=True, max_length=20)
+    password=models.CharField(blank=True, max_length=20)
     
